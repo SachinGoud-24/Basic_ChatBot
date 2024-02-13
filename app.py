4fcbd35bd4121121f7aeb5d71596f94319751802
Sachin Goud <sachin_goud@Sachins-MacBook-Air.local>
2024-02-13 18:44:39 +0530

    working files upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langchain_openai import OpenAI, ChatOpenAI
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
+import os
+
+
+if 'cont_context' not in st.session_state:
+    st.session_state['cont_context'] = [
+        SystemMessage(content = 'You are an assistant to a smart, curious and educated person. You always answer with a stern voice, and keep the message to the point')
+    ]
+
+def get_llm_response(question):
+    st.session_state['cont_context'].append(HumanMessage(content=question))
+    response = chat(st.session_state['cont_context'])
+    st.session_state['cont_context'].append(AIMessage(content=question))
+    return response.content
+
+# get openai_api_key
+load_dotenv()
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+chat = ChatOpenAI(temperature=0.5,model='gpt-3.5-turbo')
+
+# Streamlit webpage customisation
+st.set_page_config(page_title='Your Friendly Chatbot', layout='wide')
+
+st.header('Just a friendly chatbot')
+q = st.text_input("Ask me anything: ",key="input")
+if st.button('Generate response'):
+    st.text('Here is what I got :')
+    st.write(get_llm_response(q))
+
+
+
